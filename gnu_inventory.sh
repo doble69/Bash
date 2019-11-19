deb3872715dde8c092a5e91940806ee8ba4ee882
Renzo Andrea Montuori <renzo.a.montuori@gmail.com>
2019-11-19 13:05:42 -0300

    Update gnu_inventory.sh

--- gnu_inventory.sh
+++ gnu_inventory.sh
@@ -1,13 +1,14 @@
 echo "CPU"
-lscpu
+cat /proc/cpuinfo
 echo "Memory"
 cat /proc/meminfo
-echo "System"
-uname
+echo "System Unique ID"
+dmidecode -s system-uuid
+echo "Hostname"
 echo "Kernel"
 uname -r
 echo "Distro"
-uname -v
+lsb_release -a
 echo "Ntw.Board - IP - Mac Address"
 ip addr show | awk 'function IPMAC() {if (link>"") {printf "%s - %s - %s\n", iface, inets, link}}
         $0 ~ /^[1-9]/ {IPMAC();iface=substr($2, 1, index($2, ":")-1);inets="";link=""}
